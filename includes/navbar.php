9db9bd70ebc59b9a2655d03b70c1129f6a967a68
Nicodemus Opon <nicodemusopon@gmail.com>
2019-02-22 22:28:13 +0300

    2019-02-22T22:28

--- includes/navbar.php
+++ includes/navbar.php
@@ -18,21 +18,22 @@
         <div class="collapse navbar-collapse" data-nav-imagei="assets/img/blurred-image-1.jpg" data-color="orange">
             <ul class="navbar-nav ml-auto">
 
-                <?php
-                $ki= "$_SERVER[REQUEST_URI]";
-
-                if ($ki!=="/" or $ki!=="/index.php"){
-                    echo '<li class="nav-item dropdown">
-                    <a href="index.php" class="nav-link dropdown-togglev" id="navbarDropdownMenuLink1" >
-                        <i class="now-ui-icons arrows-1_minimal-left"></i>
-                        <p>HOME</p>
-                            
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-expanded="false">
+                        <i class="now-ui-icons design_app"></i>
+                        <p>Products</p>
                     </a>
-
-                </li>';
-                }
-                ?>
-
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink1">
+                        <a class="dropdown-item" href="./index.html">
+                            <i class="now-ui-icons business_chart-pie-36"></i>
+                            All components
+                        </a>
+                        <a class="dropdown-item" target="_blank" href="./docs/1.0/getting-started/introduction.html">
+                            <i class="now-ui-icons design_bullet-list-67"></i>
+                            Documentation
+                        </a>
+                    </div>
+                </li>
 
                 <li class="nav-item dropdown">
                     <a href="browse.php" class="nav-link dropdown-togglev" id="navbarDropdownMenuLink">
@@ -42,39 +43,10 @@
 
                 </li>
 
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">
-                        <i class="now-ui-icons ui-1_zoom-bold" aria-hidden="true"></i>
-                        <p>VIEW CARS BY</p>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-
-                        <a class="dropdown-item" href="index.php#manufacturers">
-                            <i class="now-ui-icons shopping_delivery-fast"></i>
-                            MANUFACTURERS
-                        </a>
-                        <a class="dropdown-item" href="categories.php">
-                            <i class="now-ui-icons shopping_delivery-fast"></i>
-                            CATEGORIES
-                        </a>
-
-
-                    </div>
-                </li>
-
-
-
-                <li class="nav-item dropdown">
-                    <a href="index.php#contactx" class="nav-link dropdown-togglev" id="navbarDropdownMenuLink1" >
-                        <i class="now-ui-icons ui-1_email-85"></i>
-                        <p>CONTACT US</p>
-                    </a>
-
-                </li>
 
                 <li class="nav-item">
                     <a class="nav-link btn btn-neutral btn-round ccc" href="tel:0733626262" target="_blank">
-                        <p><i class="now-ui-icons tech_mobile"></i> CALL US +(254) 733 626 262</p>
+                        <p><i class="now-ui-icons tech_mobile"></i> Sign Up</p>
                     </a>
                 </li>
 
