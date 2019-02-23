b5eea35ea7886e72a570800c47d96740f473703c
Nicodemus Opon <nicodemusopon@gmail.com>
2019-02-23 18:34:38 +0300

    2019-02-23T18:34

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,7 +5,7 @@
 
         <div class="navbar-translate">
             <a class="navbar-brand" href="index.php" rel="tooltip" title="GariMax Motors Ltd Home Page" data-placement="bottom" >
-                <img class="img " src="./assets/img/favicon.png" style="height: 25px"> <b> Finobu</b>
+                <img class="img " src="../assets/img/favicon.png" style="height: 25px"> <b> Finobu</b>
             </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
