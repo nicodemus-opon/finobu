b3dda7374d75487dc1a1af02428a1655b766c2c1
Nicodemus Opon <nicodemusopon@gmail.com>
2019-02-23 19:23:57 +0300

    2019-02-23T19:23

--- includes/head.php
+++ includes/head.php
@@ -25,28 +25,24 @@
 
     <title>Finobu | Finance tools for SME's</title>
     <meta charset="utf-8"/>
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicoon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.png">
     <link rel="icon" type="image/png" href="./assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
 
     <!--  Social tags      -->
     <meta name="keywords"
-          content="Garimax, Gari Max, Car dealership Kenya, used cars,cheap,quality,garimax motors,cars,kenya,garimax motors ltd">
+          content="Finobu,software tools for sme,finance tools">
 
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet"/>
-    <!-- NOW UI Kit CSS -->
+
     <link rel="stylesheet" href="../assets/css/now-ui-kit.min.css">
     <link rel="stylesheet" href="../assets/aos.css">
     <link href="../assets/hover/css/hover-min.css" rel="stylesheet">
-    <!--Start of Tawk.to Script-->
-    <!--link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"-->
 
-    <!--Start of Tawk.to Script-->
     <script type="text/javascript">
         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
         (function(){
@@ -58,7 +54,7 @@
             s0.parentNode.insertBefore(s1,s0);
         })();
     </script>
-    <!--End of Tawk.to Script-->
+
     
 </head>
 
