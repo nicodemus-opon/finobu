93e3a64d30c06b880e65beb6f6d11a570254528a
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-01 11:47:36 +0300

    2019-03-01T11:47

--- includes/head.php
+++ includes/head.php
@@ -2,15 +2,14 @@
 <html lang="en">
 
 <head>
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@finobutools" />
-    <meta name="twitter:creator" content="@finobutools" />
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:site" content="@finobutools"/>
+    <meta name="twitter:creator" content="@finobutools"/>
     <meta property="og:title" content="">
     <meta property="og:description" content="">
     <meta property="og:image" content="https://finobu.com/assets/img/logo2.png">
     <meta property="og:url" content="">
     <meta name="twitter:card" content="">
-
 
     <script type="application/ld+json">
         {
@@ -22,7 +21,10 @@
             "logo": "https://www.finobu.com/assets/img/logo2.png",
             "url": "https://www.finobu.com",
             "telephone": "0727680698",
-            "sameAs": ["https://twitter.com/finobutools","https://facebook.com/finobu"],
+            "sameAs": [
+                "https://twitter.com/finobutools",
+                "https://facebook.com/finobu"
+            ],
             "address": {
                 "@type": "PostalAddress",
                 "streetAddress": "Nairobi, Kenya",
@@ -34,7 +36,8 @@
     </script>
 
     <title>Finobu | Finance tools for SME's</title>
-    <meta name="Description" content="Finobu offers a range of finance tools for small and medium enterprises. ranging from freelance businesses,saloons,shops ,clinics">
+    <meta name="Description"
+          content="Finobu offers a range of finance tools for small and medium enterprises. ranging from freelance businesses,saloons,shops ,clinics">
     <meta charset="utf-8"/>
     <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.png">
     <link rel="icon" type="image/png" href="./assets/img/favicon.png">
@@ -55,34 +58,63 @@
     <link href="../assets/hover/css/hover-min.css" rel="stylesheet">
 
     <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/5c5d92696cb1ff3c14cba91c/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/5c5d92696cb1ff3c14cba91c/default';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
         })();
     </script>
-<style>
-    .flot {
-        -webkit-animation: action 1s infinite  alternate;
-        animation: action 1s infinite  alternate;
-    }
-    @-webkit-keyframes action {
-        0% { transform: translateY(0); }
-        100% { transform: translateY(-10px); }
-    }
-    @keyframes action {
-        0% { transform: translateY(0); }
-        100% { transform: translateY(-10px); }
-    }
-</style>
-    
+
+    <script>
+        window['_fs_debug'] = false;
+        window['_fs_host'] = 'fullstory.com';
+        window['_fs_org'] = 'JQ1EH';
+        window['_fs_namespace'] = 'FS';
+        (function(m,n,e,t,l,o,g,y){
+            if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
+            g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
+            o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
+            y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+            g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
+            g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};
+            g.consent=function(a){g("consent",!arguments.length||a)};
+            g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+            g.clearUserCookie=function(){};
+        })(window,document,window['_fs_namespace'],'script','user');
+    </script>
+
+    <style>
+        .flot {
+            -webkit-animation: action 1s infinite alternate;
+            animation: action 1s infinite alternate;
+        }
+
+        @-webkit-keyframes action {
+            0% {
+                transform: translateY(0);
+            }
+            100% {
+                transform: translateY(-10px);
+            }
+        }
+
+        @keyframes action {
+            0% {
+                transform: translateY(0);
+            }
+            100% {
+                transform: translateY(-10px);
+            }
+        }
+    </style>
+
 </head>
 
 
-<body class="" >
+<body class="">
     
     
