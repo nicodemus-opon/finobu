db60be0cc78a8cf016b640060b45bf98cfcb838b
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-02 15:19:17 +0300

    2019-03-02T15:19

--- projects/index.php
+++ projects/index.php
@@ -13,7 +13,7 @@ include('./../includes/navbar.php');
             <div class="row">
                 <div class="col-md-5">
                     <div class="card-image">
-                        <img class="img img-raised rounded" src="assets/img/examples/card-blog4.jpg">
+                        <img class="img img-raised rounded" src="./../assets/img/examples/card-blog4.jpg">
                     </div>
                 </div>
                 <div class="col-md-7">
@@ -31,30 +31,6 @@ include('./../includes/navbar.php');
             </div>
         </div>
 
-        <div class="card card-plain card-blog">
-            <div class="row">
-                <div class="col-md-7">
-                    <h6 class="category text-danger">
-                        <i class="now-ui-icons now-ui-icons media-2_sound-wave"></i> Startup
-                    </h6>
-                    <h3 class="card-title">
-                        <a href="#pablo">Insticator raises $5.2M to help publishers</a>
-                    </h3>
-                    <p class="card-description">
-                        Insticator is announcing that it has raised $5.2 million in Series A funding. The startup allows online publishers to add quizzes, polls and other interactive elements (either created by Insticator or by the publisher themselves) to their stories.<a href="#pablo"> Read More </a>
-                    </p>
-                    <p class="author">
-                        by <a href="#pablo"><b>Anthony Ha</b></a>, 5 days ago
-
-                    </p></div>
-                <div class="col-md-5">
-                    <div class="card-image">
-                        <img class="img img-raised rounded
-                                " src="assets/img/examples/card-blog6.jpg">
-                    </div>
-                </div>
-            </div>
-        </div>
 
     </div>
 </div>
