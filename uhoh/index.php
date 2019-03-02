d1ea0fe5872016a37b80e9b6e51d50a94a66089e
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-02 21:07:04 +0300

    2019-03-02T21:07

--- uhoh/index.php
+++ uhoh/index.php
@@ -25,7 +25,8 @@ include('../includes/head.php');
         </div>
         <div class="row text-center">
             <div class="col-4"></div>
-            <div class="col-4"><p class="title">check the url you provided or</p> <a class="btn btn-primary btn-round" href="<?php echo '//' . $_SERVER['SERVER_NAME'] .''?>"> Go back Home</a></div>
+            <div class="col-4"><p class="title">check the url you provided or</p>
+                <a class="btn btn-primary btn-round" href="<?php echo '//' . $_SERVER['SERVER_NAME'] .''?>"> ← Go back Home</a></div>
             <div class="col-4"></div>
 
 
