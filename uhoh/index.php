22d64997a0c6e300fbe62e04b70cf53d9bdc032b
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-02 20:41:56 +0300

    2019-03-02T20:41

--- uhoh/index.php
+++ uhoh/index.php
@@ -1,12 +1,12 @@
 <title>Finobu | Pricing</title>
 <?php
 include('../includes/head.php');
-include('../includes/navbar.php');
+
 ?>
 
-<div class="pricing-2">
+<div class="pricing-2 section p-5">
 
-    <div class="container">
+    <div class="container ">
         <div class="row">
             <div class="col-md-6 ml-auto mr-auto text-center">
                 <h2 class="title">Pick the best plan for you</h2>
