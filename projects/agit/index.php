9378fa33b07d4b4cfa1bda0d6e0fb1d7ed06097b
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-02 13:41:36 +0300

    2019-03-02T13:41

--- projects/agit/index.php
+++ projects/agit/index.php
@@ -3,7 +3,7 @@
 include('../../includes/head.php');
 include('../../includes/navbar.php');
 ?>
-<div class="container section p-5 d-flex text-center" >
+<div class="container section " >
 <div id="header">
 
 </div><!-- end header -->
