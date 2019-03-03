d820ba554d3b7eb63c17a90fdd750aae77f475a2
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-03 09:34:21 +0300

    2019-03-03T09:34

--- affiliates/index.php
+++ affiliates/index.php
@@ -25,7 +25,7 @@ include('../includes/navbar.php');
                         <h3 class="card-title">Sign up free </h3>
                         <p class="card-description">
                             This is good if your company size is between 100 and 299 employees.</p>
-                        <a href="#pablo" class="btn btn-primary btn-round">Add to Cart</a>
+                        
                     </div>
                 </div>
             </div>
@@ -38,7 +38,7 @@ include('../includes/navbar.php');
                         <h3 class="card-title">Write about us</h3>
                         <p class="card-description">
                             This is good if your company size is between 100 and 299 employees.</p>
-                        
+
                     </div>
                 </div>
             </div>
