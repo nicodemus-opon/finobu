2f568673ef723cd47b31b2649f5d6d469a378c0a
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-03 09:12:23 +0300

    2019-03-03T09:12

--- affiliates/index.php
+++ affiliates/index.php
@@ -14,7 +14,7 @@ include('../includes/navbar.php');
             </div>
         </div>
 
-        <div class="row ml-auto mr-auto">
+        <div class="row ml-auto mr-auto text-center">
             <div class="col-md-6 col-lg-4">
                 <div class="card card-pricing">
                     <div class="card-body">
