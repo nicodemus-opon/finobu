0bde467bb0724554f3c4377563c7c1f9b8545007
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-03 08:16:43 +0300

    2019-03-03T08:16

--- uhoh/index.php
+++ uhoh/index.php
@@ -6,7 +6,7 @@ include('../includes/head.php');
 
 <div class="pricing-2 section p-5">
 
-    <div class="container ">
+    <div class="container container-fluid">
         <div class="row">
             <div class="col-md-6 ml-auto mr-auto text-center">
                 <h2 class="title">Well stuff happens</h2>
@@ -15,19 +15,19 @@ include('../includes/head.php');
         </div>
 
         <div class="row text-center">
-            <div class="col-4"></div>
-            <div class="col-4"><img wnidth="760px" hbeight="600px" class="img img-responsive"
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"><img wnidth="760px" hbeight="600px" class="img img-responsive"
                                     src="./../assets/fogg/fogg-fatal-error-3.png"
                                     srci="assets/fogg/fogg-"></div>
-            <div class="col-4"></div>
+            <div class="col-lg-4"></div>
 
 
         </div>
         <div class="row text-center">
-            <div class="col-4"></div>
-            <div class="col-4"><p class="title">check the url you provided or</p>
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"><p class="title">check the url you provided or</p>
                 <a class="btn btn-primary btn-round" href="<?php echo '//' . $_SERVER['SERVER_NAME'] .''?>"> ← Go back Home</a></div>
-            <div class="col-4"></div>
+            <div class="col-lg-4"></div>
 
 
         </div>
