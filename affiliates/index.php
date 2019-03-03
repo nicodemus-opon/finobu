74c640ebd4e6c53d64c91d692117b18c6d163781
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-03 09:10:02 +0300

    2019-03-03T09:10

--- affiliates/index.php
+++ affiliates/index.php
@@ -15,7 +15,20 @@ include('../includes/navbar.php');
         </div>
 
         <div class="row">
-
+            <div class="col-md-6 col-lg-3">
+                <div class="card card-pricing">
+                    <div class="card-body">
+                        <h6 class="category">Alpha Pack</h6>
+                        <div class="icon icon-primary">
+                            <i class="now-ui-icons objects_diamond"></i>
+                        </div>
+                        <h3 class="card-title">$67</h3>
+                        <p class="card-description">
+                            This is good if your company size is between 100 and 299 employees.</p>
+                        <a href="#pablo" class="btn btn-primary btn-round">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
