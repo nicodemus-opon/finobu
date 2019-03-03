880500b264039e5e0684ea29036747b38e172fd6
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-03 08:21:31 +0300

    2019-03-03T08:21

---
+++ careers/index.php
@@ -0,0 +1,103 @@
+<title>Finobu | Pricing</title>
+<?php
+include('../includes/head.php');
+include('../includes/navbar.php');
+?>
+
+<div class="pricing-2">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 ml-auto mr-auto text-center">
+                <h2 class="title">Pick the best plan for you</h2>
+
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-md-4">
+                <div class="card card-pricing card-plaini">
+                    <div class="card-body">
+                        <h6 class="category">Free</h6>
+                        <h1 class="card-title">
+                            <small>kes</small>
+                            0
+                        </h1>
+                        <ul>
+
+                            <li><b>Unlimited</b> Jibu Forms</li>
+                            <li><b>10</b> Solo Invoices</li>
+                            <li><b>Unlimited</b> Solo Proposals</li>
+                            <li><b>3</b> Solo Projects</li>
+                            <li><b>5</b> Solo Clients</li>
+                            <li><b>1 GB</b> Storage</li>
+
+                        </ul>
+                        <a href="#pablo" class="btn btn-primary btn-round">
+                            Sign Up
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card card-pricing card-backgroundi card-raised"
+                     style="background-imagei: url('../assets/img/pricing2.jpg')">
+                    <div class="card-body">
+                        <h6 class="category text-info">Individual</h6>
+                        <h1 class="card-title">
+                            <small>kes</small>
+                            29
+                        </h1>
+
+                        <ul>
+
+                            <li><b>Unlimited</b> Jibu Forms</li>
+                            <li><b>Unlimited</b> Solo Invoices</li>
+                            <li><b>Unlimited</b> Solo Proposals</li>
+                            <li><b>Unlimited</b> Solo Projects</li>
+                            <li><b>Unlimited</b> Solo Clients</li>
+                            <li><b>10 GB</b> Storage</li>
+
+                        </ul>
+                        <a href="#pablo" class="btn btn-primary btn-round">
+                            Start free trial
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card card-pricing card-plaini">
+                    <div class="card-body">
+                        <h6 class="category">Standard</h6>
+                        <h1 class="card-title">
+                            <small>kes</small>
+                            17
+                        </h1>
+
+                        <ul>
+                            <li><b>2GB</b> Disk Space</li>
+                            <li><b>25GB</b> Monthly Bandwidth</li>
+                            <li><b>5</b> Email Accounts</li>
+                            <li><b>Unlimited</b> subdomains</li>
+                        </ul>
+                        <a href="#pablo" class="btn btn-primary btn-round">
+                            Start free trial
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+include('../includes/footer.php');
+include('../includes/end.php');
+
+?>
