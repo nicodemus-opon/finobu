75c27ec62879b86674e8e36e0c77e87ba3d7e2b5
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-07 08:37:47 +0300

    2019-03-07T08:37

--- pricing/solo/index.php
+++ pricing/solo/index.php
@@ -1,3 +1,4 @@
 <script src="https://gumroad.com/js/gumroad-embed.js"></script>
-<div class="gumroad-product-embed" data-gumroad-product-id="WrZQT"><a href="https://gumroad.com/l/WrZQT">Loading...</a>
+<div class="gumroad-product-embed" data-gumroad-product-id="WrZQT">
+    <a href="https://gumroad.com/l/WrZQT">Loading...</a>
 </div>
