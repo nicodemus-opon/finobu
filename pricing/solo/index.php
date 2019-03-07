fe0fa87d258793ff2dacac004737d7f09365e598
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-07 08:39:54 +0300

    2019-03-07T08:39

--- pricing/solo/index.php
+++ pricing/solo/index.php
@@ -3,10 +3,11 @@
 include('../../includes/head.php');
 include('../../includes/navbar.php');
 ?>
-
+<div class="section">
 <script src="https://gumroad.com/js/gumroad-embed.js"></script>
 <div class="gumroad-product-embed" data-gumroad-product-id="WrZQT">
     <a href="https://gumroad.com/l/WrZQT">Loading...</a>
+</div>
 </div>
 <?php
 include('../../includes/footer.php');
