1c68b0798153e4038e9a4586c5bd36ff8ab27ba6
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-07 08:39:23 +0300

    2019-03-07T08:39

--- pricing/solo/index.php
+++ pricing/solo/index.php
@@ -1,4 +1,15 @@
+<title>Finobu | Pricing</title>
+<?php
+include('../../includes/head.php');
+include('../../includes/navbar.php');
+?>
+
 <script src="https://gumroad.com/js/gumroad-embed.js"></script>
 <div class="gumroad-product-embed" data-gumroad-product-id="WrZQT">
     <a href="https://gumroad.com/l/WrZQT">Loading...</a>
 </div>
+<?php
+include('../../includes/footer.php');
+include('../../includes/end.php');
+
+?>
