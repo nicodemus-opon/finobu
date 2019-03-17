3045aee58729ab472d5d5ff22bac800e48919f47
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-17 09:49:35 +0300

    2019-03-17T09:49

--- includes/head.php
+++ includes/head.php
@@ -62,7 +62,7 @@
             font-family: 'heebo';
         }
     </style-->
-    <script type="text/javascript">
+    <!--script type="text/javascript">
         var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         (function () {
             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
@@ -72,8 +72,35 @@
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
         })();
-    </script>
+    </script-->
+    <!-- Start of Async Drift Code -->
+    <script>
+        "use strict";
 
+        !function() {
+            var t = window.driftt = window.drift = window.driftt || [];
+            if (!t.init) {
+                if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+                t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+                    t.factory = function(e) {
+                        return function() {
+                            var n = Array.prototype.slice.call(arguments);
+                            return n.unshift(e), t.push(n), t;
+                        };
+                    }, t.methods.forEach(function(e) {
+                    t[e] = t.factory(e);
+                }), t.load = function(t) {
+                    var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+                    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+                    var i = document.getElementsByTagName("script")[0];
+                    i.parentNode.insertBefore(o, i);
+                };
+            }
+        }();
+        drift.SNIPPET_VERSION = '0.3.1';
+        drift.load('7dmh6hckhxk9');
+    </script>
+    <!-- End of Async Drift Code -->
     <script>
         window['_fs_debug'] = false;
         window['_fs_host'] = 'fullstory.com';
