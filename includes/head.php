0c73ae16a991ccf0e44c60661485f328ac250747
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-20 15:21:51 +0300

    2019-03-20T15:21

--- includes/head.php
+++ includes/head.php
@@ -118,21 +118,7 @@
             }
         }
     </style>
-    <!-- HoverSignal -->
-    <script type="text/javascript" >
-        (function (d, w) {
-            var n = d.getElementsByTagName("script")[0],
-                s = d.createElement("script"),
-                f = function () { n.parentNode.insertBefore(s, n); };
-            s.type = "text/javascript";
-            s.async = true;
-            s.src = "https://app.hoversignal.com/Api/Script/5933bd64-eb78-4461-9ff0-44c5e21c695b";
-            if (w.opera == "[object Opera]") {
-                d.addEventListener("DOMContentLoaded", f, false);
-            } else { f(); }
-        })(document, window);
-    </script>
-    <!-- /Hoversignal -->
+   
 </head>
 
 
