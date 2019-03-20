ddd5e863556376c5ee3222974299924a98c1a0f7
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-20 09:25:33 +0300

    2019-03-20T09:25

--- templates/first.php
+++ templates/first.php
@@ -1,4 +1,4 @@
-<div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/duot.png');background-position: center;background-size:cover">
+<div class="page-header header-filterm" data-parallax="true" style="background-image: url('assets/duot.png');background-position: center;background-size:cover">
     <div class="page-header-imageb" style="background-image: url('assets/img/bg14.jpg');"></div>
     <div class="container ">
         <div class="row">
