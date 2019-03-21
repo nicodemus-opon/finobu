36c1caf59990a5eef2acda64ade1e5e39a73a47d
Nicodemus Opon <nicodemusopon@gmail.com>
2019-03-21 09:50:29 +0300

    2019-03-21T09:50

--- solo/index.php
+++ solo/index.php
@@ -13,7 +13,7 @@ include('./../includes/navbar.php');
                     <p> the ultimate all in one tool for freelancers built by freelacers</p>
 
                     <div class="input-group mb-3">
-                        <a class="btn btn-primary btn-neutrali" role="button" href="./beta">Join Beta</a>
+                        <a class="btn btn-primary btn-neutrali" role="button" href="../beta">Join Beta</a>
                     </div>
                 </div>
             </div>
